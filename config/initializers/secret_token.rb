2e201ac732c7c38c3048d808987e5e7f3f74bc53
Ranjith <ranjithru@gmail.com>
2011-09-06 20:51:12 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hds4u::Application.config.secret_token = '35226a9bb1d0379fabdfe7b599b204c4a8c76b3e4568ec773913afa3c3096a94f70a4fec9d2a6762b7b71fa5f5864b7ab22cc28e99f98ae40b12861e508c579e'
