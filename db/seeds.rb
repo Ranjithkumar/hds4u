f92ba7c77e9f8a009f2723a1060f8c7d1e5e6332
Ranjith <ranjithru@gmail.com>
2011-09-12 07:29:55 +0530

    Role modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+if Role.count == 0
+  Role.create(:title => "Admin")
+  Role.create(:title => "Technician")
+  Role.create(:title => "Business user")
+end
+
 if User.count == 0
-  admin = User.new(:email => "ranjithru@gmail.com", :password => "rans", :password_confirmation => "rans", :user_name => "Rans", :name => "Ranjithkumar", :employee_id => 101)
+  admin = User.new(:email => "ranjithru@gmail.com", :password => "rans", :password_confirmation => "rans", :user_name => "Rans", :name => "Ranjithkumar", :employee_id => 101, :super_admin => true, :role_id => 1)
   admin.save
 end
